5d9d049f02d2814f655ef325c1a439dd31501220
dima zub <dimzubenko@gmail.com>
2021-01-28 15:56:16 +0300

    homework-2

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import s from './header.module.css';
+
+const Header = ({ title, descr}) => {
+    return(
+        <div className={s.root}>
+            <div className={s.forest}></div>
+            <div className={s.container}>
+                    <h1>{ title }</h1>
+                    <p>{ descr }</p>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
